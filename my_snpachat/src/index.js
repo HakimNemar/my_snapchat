1f6295087b1eea5d90a1beb2bdc87e540a155107
hakim-nemar <hakim.nemar@epitech.eu>
2020-05-26 22:09:21 +0200

    test show hide register login

--- my_snpachat/src/index.js
+++ my_snpachat/src/index.js
@@ -1,116 +1,143 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Test(props) {
-  return <h1>Bonjour, {props.name}</h1>;
-}
-
-const test = <Test name="Sara" />;
-
-//------------------------------------------------------------------------------------------
-
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-  return (
-    <img
-      className="Avatar"
-      src = {props.user.avatarUrl}
-      alt = {props.user.name}
-    />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className = "UserInfo">
-      <Avatar user = {props.user} />
-      <div className = "UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className = "Comment">
-      <UserInfo user = {props.author} />
-      <div className = "Comment-text">{props.text}</div>
-      <div className = "Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
-};
-
-ReactDOM.render(
-  // <App />,
-  // test,
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />,
-  document.getElementById('root')
-);
-
-//--------------------------------------------------------------------------------------------
-
-class Clock extends React.Component {
+class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {
+      email: '',
+      password: '',
+      cpassword: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  handleSubmit(event) {
+    // alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+    console.log(this.state);
   }
 
   render() {
     return (
-      <div>
-        <h1>Bonjour, monde !</h1>
-        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
+      <div className="container my-5 text-center">
+        <form onSubmit={this.handleSubmit} className="forminscri">
+          <div className="form-group">
+            <label className="white">
+              Email:
+              <input type="text" className="form-control" name="email" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="form-group">
+            <label className="white">
+              Password:
+              <input type="password" className="form-control" name="password" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="form-group">
+            <label className="white">
+              Confirm Password:
+              <input type="password" className="form-control" name="cpassword" onChange={this.handleChange} />
+            </label>
+          </div>
+          <small className="form-text text-muted white">Already have an account ? login</small>
+          <button type="submit" className="btn btn-warning white">Submit</button>
+        </form>
       </div>
     );
   }
 }
 
-// function tick() {
-//   ReactDOM.render(
-//     <Clock />,
-//     document.getElementById('root')
-//   );
-// }
-// 
-// setInterval(tick, 1000);
+// -------------------------------------------------------------------------------------------
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {
+      isNoteState: false
+    };
+  }
+
+  handleLoginClick() {
+    this.setState({isNoteState: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isNoteState: false});
+  }
+
+  render() {
+    const isNoteState = this.state.isNoteState;
+
+    return (
+      <div>
+        <Greeting isNoteState={isNoteState} />
+      </div>
+    );
+  }
+}
+
+function Greeting(props) {
+  const isNoteState = props.isNoteState;
+  if (isNoteState) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+function UserGreeting(props) {
+  return <button>Login</button>;
+}
+
+function GuestGreeting(props) {
+  return (<div>
+            <button onClick={props.onClick}>Sign up</button>
+            <button onClick={props.onClick}>Login</button>
+          </div>);
+}
+
+function FromIn(props) {
+  return <h1>From Inscription</h1>
+}
+
+function Fromlog(props) {
+  return <h1>From Login</h1>
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Sign up
+    </button>
+  );
+}
+
+// -------------------------------------------------------------------------------------------
+
+ReactDOM.render(
+  // <App />,
+  <NameForm />,
+  // <LoginControl />,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
