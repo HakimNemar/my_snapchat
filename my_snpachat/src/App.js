8b32458b0f33f8cb8c2fd56cf1e389dc29f47242
hakim-nemar <hakim.nemar@epitech.eu>
2020-05-27 20:38:08 +0200

    can send snap and show

--- my_snpachat/src/App.js
+++ my_snpachat/src/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+// import Register from './components/loginRegister/register/register.component';
+import Register from './components/snap/sendSnap';
+import SnapIndex from './components/snap/sendSnap';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
+    <Register />
   );
 }
 
